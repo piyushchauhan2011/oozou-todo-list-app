99b51684604d1abc2204c710ea311980c4f8af4b
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2021-08-30 05:57:28 +0700

    feat: add a getting all todo route

---
+++ apps/api/src/app/routes/todos/get-list.route.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import { Response } from 'express-serve-static-core';
+import { IResponseBody } from '@todolist/shared';
+import { todoRepository } from '../../repositories';
+import { SUCCESS_MESSAGE } from '../../constants';
+
+const getTodoList = express();
+
+getTodoList.get('todos', async (req, res: Response<IResponseBody>, next) => {
+  try {
+    const repo = await todoRepository();
+    const results = await repo.createQueryBuilder('todos').getMany();
+    res.json({
+      status: { message: SUCCESS_MESSAGE, code: res.statusCode },
+      data: results,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+export default getTodoList;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,27 @@
 import * as express from 'express';
-// import * as cors from 'cors';
+import * as cors from 'cors';
 import {
   createTodoRoute,
   updateTodoStatusRoute,
-  createSubtaskRoute, updateSubtaskStatusRoute
+  createSubtaskRoute,
+  updateSubtaskStatusRoute,
+  getTodoList,
 } from './app/routes';
 import { Response } from 'express-serve-static-core';
 import { IResponseBody } from '@todolist/shared';
 import { NextFunction } from 'express';
 import { BASE_URL, SUBTASK_URL, TODO_URL } from './app/constants';
-// import { corsOptions as options } from './app/configs';
+import { corsOptions as options } from './app/configs';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(options))
 app.use(TODO_URL, createTodoRoute);
 app.use(TODO_URL, updateTodoStatusRoute);
+app.use(TODO_URL, getTodoList);
 app.use(SUBTASK_URL, createSubtaskRoute);
-app.use(SUBTASK_URL, updateSubtaskStatusRoute)
+app.use(SUBTASK_URL, updateSubtaskStatusRoute);
 app.use(
   (err: Error, req, res: Response<IResponseBody>, _next: NextFunction) => {
     res.status(500).json({
