19978c6554062c108381fe4da3593ea150822705
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2021-09-01 00:36:06 +0700

    refactor: remove environments

--- apps/api/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// When building for production, this file is replaced with `environment.prod.ts`.
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-ts-comment
-// @ts-ignore
-const environment = {
-  production: false,
-}
--- apps/my-todo-list/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-ts-comment
-// @ts-ignore
-const environment = {
-  production: true,
-}
